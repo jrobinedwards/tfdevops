191fc60876a83cdc85301901a51c5d9346221296
Robin Edwards <robin@visionsinwebs.com>
2025-07-10 10:25:01 +0200

    add data sources for ami and zones

--- conf.tf
+++ conf.tf
@@ -21,3 +21,24 @@ provider "aws" {
     }
   }
 }
+
+# Data source for latest Amazon Linux AMI
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+# Data source for availability zones
+data "aws_availability_zones" "available" {
+  state = "available"
+}
