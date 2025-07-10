9918d05d9978b13662812964bf879c8471da754f
Robin Edwards <robin@visionsinwebs.com>
2025-07-10 10:50:19 +0200

    allow only https

--- modules/security-group/main.tf
+++ modules/security-group/main.tf
@@ -3,39 +3,12 @@ resource "aws_security_group" "web_sg" {
   description = "Security group for ${var.environment} web servers"
   vpc_id      = var.vpc_id
 
-  # HTTP access
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   # HTTPS access
   ingress {
     description = "HTTPS"
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # SSH access (restricted to VPC)
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.vpc_cidr_block]
-  }
-
-  # All outbound traffic
-  egress {
-    description = "All outbound"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
