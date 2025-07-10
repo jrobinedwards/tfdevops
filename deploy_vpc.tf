19131dd12f19260361b06a0c5e97c71b6dc6e645
Robin Edwards <robin@visionsinwebs.com>
2025-07-10 09:14:44 +0200

    Add tags and improve readability

--- deploy_vpc.tf
+++ deploy_vpc.tf
@@ -12,17 +12,31 @@ resource "aws_subnet" "public-subnet" {
   vpc_id     = aws_vpc.public-vpc.id
   cidr_block = "10.0.0.0/28"
   availability_zone = "eu-west-2c"
+  
+  tags = {
+    Name = "tf-deploy-public-subnet"
+    Type = "public"
+  }
 }
 
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.public-vpc.id
+  
+  tags = {
+    Name = "tf-deploy-igw"
+  }
 }
 
 resource "aws_route_table" "default-routes" {
   vpc_id = aws_vpc.public-vpc.id
+  
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
+  }
+  
+  tags = {
+    Name = "tf-deploy-public-rt"
   }
 }
 
